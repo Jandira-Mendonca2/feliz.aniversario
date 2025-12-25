7bcf42b524c575bbb8b729c1b645678e0cfd43bf
Jandira Mendonça <152594040+Jandira-Mendonca2@users.noreply.github.com>
2025-12-25 13:57:25 +0100

    Update countdown target time to December 25, 2025 14:05

--- feliz aniversario/js/index.js
+++ feliz aniversario/js/index.js
@@ -3,7 +3,7 @@ const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 
-const targetDate = new Date('December 25 2025 00:00:00').getTime();
+const targetDate = new Date('December 25 2025 14:05:00').getTime();
 
 function timer(){
     const currentDate = new Date().getTime();
@@ -42,3 +42,4 @@ timer();
 const x = setInterval(timer, 1000);
 
 // setInterval(timer, 1000)
+
